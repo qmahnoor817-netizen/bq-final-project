4726a18f6438da8deeee7f5af4f0ee1778c080bd
qmahnoor817-netizen <qmahnoor817@gmail.com>
2026-05-08 21:50:27 +0500

    Update main.dart

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -4,6 +4,8 @@ import 'firebase_options.dart';
 import 'auth_screen.dart';
 import 'notes_list_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,18 +15,63 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() => _isDarkMode =!_isDarkMode);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Notes App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
+      theme: _isDarkMode
+          ? ThemeData.dark().copyWith(
         useMaterial3: true,
+        cardColor: Colors.grey[850],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.deepPurple,
+        ),
+        scaffoldBackgroundColor: Colors.grey[900],
+        dialogTheme: DialogThemeData(backgroundColor: Colors.grey[850]),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+        brightness: Brightness.dark,
+        ),
+      )
+          : ThemeData.light().copyWith(
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.purple.shade700,
+        ),
+        scaffoldBackgroundColor: Colors.white,
       ),
+      localizationsDelegates: const [
+        FlutterQuillLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+      ],
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
@@ -32,12 +79,14 @@ class MyApp extends StatelessWidget {
             return const Scaffold(body: Center(child: CircularProgressIndicator()));
           }
           if (snapshot.hasData) {
-            return const NotesListScreen(); // SCREEN 2: Home
+            return NotesListScreen(
+              toggleTheme: toggleTheme,
+              isDarkMode: _isDarkMode,
+            );
           }
-          return const AuthScreen(); // SCREEN 1: Login/Signup
+          return const AuthScreen();
         },
       ),
     );
   }
 }
-
